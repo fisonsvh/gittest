0f1a7d979d89858fb8ea523db97c5bef5292aaae
fisonsvh <fison23@icloud.com>
2020-05-20 18:48:42 +0300

    first commt

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 for x in range (1, 50):
     print(x)
+    x +=1
+    https://github.com/fisonsvh/gittest.git
+    $ git config --global user.name "John Doe"
+$ git config --global user.email johndoe@example.com
