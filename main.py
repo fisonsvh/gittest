eeb69cfea57be9b2eafa3438003ff7af6ac70208
fisonsvh <fison23@icloud.com>
2020-05-20 18:55:43 +0300

    next

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 for x in range (1, 50):
     print(x)
     x +=1
-    https://github.com/fisonsvh/gittest.git
+
+
+balalayka
+async def asd
+
+
+asd(parameter_list):
+    pass
+
+
+""" https://github.com/fisonsvh/gittest.git
     $ git config --global user.name "John Doe"
-$ git config --global user.email johndoe@example.com
+$ git config --global user.email johndoe@example.com"""
