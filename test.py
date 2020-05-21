e6690bc8a1f41f28eff13fbb61925f3871c2bec5
fisonsvh <Fison23@icloud.com>
2020-05-21 15:35:38 +0300

    15-35

--- test.py
+++ test.py
@@ -10,7 +10,8 @@
 # print(odd_ball(["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"])) # False
 # print(odd_ball(["even",10,"odd",2,"even"])) # True
 
-#
+#_______________________________________________________________________
+
 # def find_sum (n):
 #     n = list(range(1, n+1))
 #     n = int(n)
@@ -18,3 +19,20 @@
 #         return sum(n)
 #
 # print(find_sum(5))
+
+
+# 3. Дан список имен. Выберите в новый список только те имена, которые состоят из 4-х букв.
+# ["Ryan", "Mark", "John", "Paul"]
+
+names = ["Ryan", "Kieran", "Mark", "John", "David", "Paul"]
+new_names = []
+def get_names (names):
+    for i in names:
+        if len(i) == 4:
+            new_names.append(i)
+    print(new_names)
+
+get_names(names)
+
+
+
