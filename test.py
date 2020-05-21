c999c4329fcee6c6e196f1a7b3c02df98964a5e7
fisonsvh <Fison23@icloud.com>
2020-05-21 15:25:44 +0300

    test

---
+++ test.py
@@ -0,0 +1,20 @@
+# def odd_ball(arr):
+#     x = arr.index('odd')
+#     for i in arr:
+#         if int(x) in arr:
+#             return True
+#         else:
+#             return False
+#
+# print(odd_ball(["even",4,"even",7,"even",55,"even",6,"even",10,"odd",3,"even"])) # True
+# print(odd_ball(["even",4,"even",7,"even",55,"even",6,"even",9,"odd",3,"even"])) # False
+# print(odd_ball(["even",10,"odd",2,"even"])) # True
+
+#
+# def find_sum (n):
+#     n = list(range(1, n+1))
+#     n = int(n)
+#     print(n)
+#         return sum(n)
+#
+# print(find_sum(5))
