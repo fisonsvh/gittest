4e1be2f2481b6e0ae565416db01ad54baeabdc91
fisonsvh <fison23@icloud.com>
2020-05-23 12:23:45 +0300

    2123

--- libs.py
+++ libs.py
@@ -1,17 +1,37 @@
-def get_count(string, symbol):
-    cnt = 0;
-    for i in string:
-        if i == symbol:
-            cnt = cnt + 1
-    return cnt
+# # def get_count(string, symbol):
+# #     cnt = 0;
+# #     for i in string:
+# #         if i == symbol:
+# #             cnt = cnt + 1
+# #     return cnt
+# #
+# #
+# # def get_len(string):
+# #     cnt = 0
+# #     for i in string:
+# #         cnt += 1
+# #     return cnt
+# #
+# #
+# # def len_name(name):
+# #     return len(name)
+#
+# import random as r
+#
+# guess = r.randint (1, 10)
+#
+# while True:
+#     question = int(input("Попробуй угадать число от 1 до 200. Вводи сюда число: "))
+#     if question == guess:
+#         anwser = input("Отлично, ты угадал! Хочешь сыграть еще?: ")
+#         if anwser == 'д' or anwser == 'да':
+#             guess = r.randint(1, 10)
+#             continue
+#         else:
+#             print("Спасибо за игру!")
+#             break
+#     elif question < guess:
+#         print('Загаданое число немного больше! Попробуй еще')
+#     elif question > guess:
+#         print('Загаданое число немного меньше! Попробуй еще!')
 
-
-def get_len(string):
-    cnt = 0
-    for i in string:
-        cnt += 1
-    return cnt
-
-
-def len_name(name):
-    return len(name)
